5d680fd7ee873df68d7b35d867eb4aa8010f5e51
Patrick <ruomungai@gmail.com>
2022-08-21 14:34:28 +0300

    commit tree 45604662ac23e9bc6006c6d55056fa1183d6fe41

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,12 @@
 require_relative './dance_module'
+require_relative './fancy_dance.rb'
 require_relative './meta_dancing_module'
+
+class Kid
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+
+    def initialize(name)
+    @name = name
+  end
+end
